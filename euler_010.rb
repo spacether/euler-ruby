b6f8f655f33b3e9351a355e9d879fb3e825159ce
Justin Black <justin.a.black@gmail.com>
2017-01-16 16:59:31 -0800

    updated problem 10 to use the sieve of eratosthenes

--- euler_010.rb
+++ euler_010.rb
@@ -1,42 +1,18 @@
 # https://projecteuler.net/problem=10
 
-class PrimeMaker
-  attr_reader :primes
-
-  def initialize
-    @primes = [2,3]
-    @k = 1
-    @pad = -1
-  end
-
-  def prime?(num)
-    upbound = (num**0.5).floor
-    @primes.each do |prime|
-      return false if num % prime == 0
-      break if prime > upbound
+def get_primes(maxnum)
+  arr = (0..maxnum).to_a
+  arr[0], arr[1] = [nil, nil]
+  ubound = (maxnum**0.5).floor
+  (2..ubound).each do |num|
+    if arr[num] != nil
+      (num*2..maxnum).step(num) { |i| arr[i] = nil }
     end
-    true
   end
-
-  def next_prime
-    currlength = @primes.length
-    while @primes.length == currlength
-      num = @k*6 + @pad
-      @primes << num if self.prime?(num)
-      if @pad == 1
-        @k += 1
-      end
-      @pad = -@pad
-    end
-    @primes.last
-  end
-
+  arr.compact
 end
+# Sieve of Eratosthenes per https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# 0.75 seconds
 
-pmaker = PrimeMaker.new
-while pmaker.primes.last < 2000000
-  pmaker.next_prime
-end
-pmaker.primes.pop
-puts pmaker.primes.inject(:+)
-# done in 3.8s
+primes = get_primes(2000000)
+puts primes.inject(:+)
